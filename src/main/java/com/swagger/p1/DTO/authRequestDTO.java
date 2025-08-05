f93bc8f6a5fcbfa27fe8cff22cdc4ac8be9e78fb
Geeta Khatri <geetakhatri746@gmail.com>
2025-08-05 23:21:44 +0530

    changes name from authRequestDTO to AuthRequestDTO

--- src/main/java/com/swagger/p1/DTO/authRequestDTO.java
+++ src/main/java/com/swagger/p1/DTO/authRequestDTO.java
@@ -1,13 +1,32 @@
 package com.swagger.p1.DTO;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-public class authRequestDTO {
+
+
+public class AuthRequestDTO {
 
    
     private String username;
     private String password;
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public AuthRequestDTO() {
+    }
+    public AuthRequestDTO(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+    
+
+
 }
