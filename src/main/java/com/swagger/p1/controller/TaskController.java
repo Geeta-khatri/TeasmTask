c270926d8f79726ad93922705c3e5543595565e7
Geeta Khatri <geetakhatri746@gmail.com>
2025-08-09 23:21:32 +0530

    Implemeted patchMapping

--- src/main/java/com/swagger/p1/controller/TaskController.java
+++ src/main/java/com/swagger/p1/controller/TaskController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -50,6 +51,11 @@ public class TaskController {
     public ResponseEntity<?> getAllTask() {
         return tservice.getAllTask();
     }
-    
-    
+
+    @PatchMapping("/status/{id}")
+    public ResponseEntity<?> patchStatus(@PathVariable Long id, @RequestBody TaskDTO tdto){
+        return tservice.StatusPatch(id,tdto);
+    }
+
+
 }
