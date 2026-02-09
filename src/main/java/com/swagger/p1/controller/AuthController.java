b46be751df45d15bb8e42f544c0aeac80fe8337a
Geeta Khatri <geetakhatri746@gmail.com>
2026-02-09 20:31:21 +0530

    corrected method in requestMatcher and RequestMapping  endpoint in AuthController

--- src/main/java/com/swagger/p1/controller/AuthController.java
+++ src/main/java/com/swagger/p1/controller/AuthController.java
@@ -6,8 +6,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.swagger.p1.Entity.Users;
@@ -25,15 +27,17 @@ public class AuthController {
 	    
 	    private  final UsersRepo urepo;
 	 
-	@PostMapping("/roleAssign")
-	public String AssignUserRole(Long id, String role) {
-		
+	@PostMapping("/roleAssign/{id}")
+	public String AssignUserRole(@PathVariable Long id,@RequestParam String role) {
+		System.out.println("id is "+id +" "+ "role is "+ role);
 		Users userExists=urepo.findById(id).orElse(null);
+		
 		if(userExists==null) {
 			return "User does not exists for roe assigning";
 		}
 		else {
 			userExists.setAssigned_role(role);
+			System.out.println("users info1 "+userExists.getAssigned_role());
 			urepo.save(userExists);
 			return "Role assigned Successfullyyyy!!";
 		}
