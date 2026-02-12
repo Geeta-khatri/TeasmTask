4845c29078352694fec16c7b48b8dd7fdf1dc1c4
Geeta Khatri <geetakhatri746@gmail.com>
2026-02-13 00:21:39 +0530

    added findByProjectOwnerId  in ProjectRepo

--- src/main/java/com/swagger/p1/repository/ProjectRepo.java
+++ src/main/java/com/swagger/p1/repository/ProjectRepo.java
@@ -2,8 +2,15 @@ package com.swagger.p1.repository;
 
 import com.swagger.p1.Entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 import java.util.Optional;
 public interface ProjectRepo  extends JpaRepository<Project,Long>{
 
     Optional<Project> findByName(String name);
+    
+    @Query(value="select * from project where asign_to_user=:id",nativeQuery=true)// nativeQuery=true or else will get error for *
+    List<Project> findByProjectOwnerId(@Param("id") Long id);
 }
