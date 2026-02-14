46147190b737248e1a911e2b2d2bd9bbcdaa16b7
Geeta Khatri <geetakhatri746@gmail.com>
2026-02-14 09:19:21 +0530

    edited endpoints for admin level

--- src/main/java/com/swagger/p1/controller/ProjectController.java
+++ src/main/java/com/swagger/p1/controller/ProjectController.java
@@ -1,10 +1,22 @@
 package com.swagger.p1.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import com.swagger.p1.DTO.*;
+import com.swagger.p1.Entity.Users;
 import com.swagger.p1.Service.ProjectService;
+import com.swagger.p1.repository.UsersRepo;
+import com.swagger.p1.security.JWTConfig;
+
+import io.jsonwebtoken.Claims;
+
+import com.swagger.p1.security.JWTConfig;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,34 +30,47 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/projects")
+@RequiredArgsConstructor
 public class ProjectController {
 
-    @Autowired
-    private ProjectService pservice;
+    private final JWTConfig jwtconfig;
+	private final UsersRepo urepo;
+    private final ProjectService pservice;
 
-    @PostMapping("/createProject")
+    @PostMapping("/admin/createProject")
     public ResponseEntity<?> projectcreate(@RequestBody ProjectDTO pdto) {
         return pservice.createProject(pdto);
     }
 
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("/admin/delete/{id}")
     public ResponseEntity<?> projectDelete(@PathVariable Long id){
         return pservice.deleteProject(id);
     }
     
-    @PutMapping("/update/{id}")
+    @PutMapping("/admin/update/{id}")
      public ResponseEntity<?> projectUpdate(@PathVariable Long id,@RequestBody ProjectDTO pdto){
         return pservice.updateProject(id,pdto);
     }
 
-    @GetMapping("/getAllProject")
+    @GetMapping("/admin/getAllProject")
     public ResponseEntity<?> getAllProject() {
         return pservice.AllProject();
     }
     
-    @GetMapping("/getProject/{id}")
-    public ResponseEntity<?> getMethodName(@PathVariable Long id) {
-        return pservice.projectById(id);
+    //getting project assigned to logged in user
+    @GetMapping("/getProject")
+    public ResponseEntity<?> projectByUserId(HttpServletRequest req) {
+    	Claims claim=jwtconfig.getClaimsDtls(req);
+    	if(claim!=null) {
+    		Users u= urepo.findByUserName(claim.getSubject()).orElse(null);
+    		if(u!=null) {
+        return pservice.projectByUserId(u.getId());
+    		}
+    		else {
+    			return new ResponseEntity<>("User does not exists",HttpStatus.BAD_REQUEST);
+    		}
+    	}
+    	return new ResponseEntity<>("Invalid Login",HttpStatus.UNAUTHORIZED);
     }
     
     
