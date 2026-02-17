a0adedb78c26130bfcb6d332be1e580697b08976
Geeta Khatri <geetakhatri746@gmail.com>
2026-02-18 02:11:24 +0530

    RefreshTokenReqDTO for

---
+++ src/main/java/com/swagger/p1/DTO/RefreshTokenReqDTO.java
@@ -0,0 +1,12 @@
+package com.swagger.p1.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class RefreshTokenReqDTO {
+	
+private String tokenMsg;
+	
+}
