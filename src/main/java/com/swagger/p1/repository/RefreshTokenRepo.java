b4e16df9d6dc506e750db5a727eedd5280e18aba
Geeta Khatri <geetakhatri746@gmail.com>
2026-02-18 02:16:01 +0530

    added query method for findByTokenMsg

--- src/main/java/com/swagger/p1/repository/RefreshTokenRepo.java
+++ src/main/java/com/swagger/p1/repository/RefreshTokenRepo.java
@@ -1,9 +1,15 @@
 package com.swagger.p1.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.swagger.p1.Entity.RefreshToken;
 
 public interface RefreshTokenRepo extends JpaRepository<RefreshToken, Long> {
 
+	@Query(value="select * from refresh_token where token_msg=:token_msg",nativeQuery=true)
+	Optional<RefreshToken> findByTokenMsg(@Param("token_msg")String tokenMsg);
 }
