d88c7e8eababf0eecc1fd402f30e6c25dda4d59c
Geeta Khatri <geetakhatri746@gmail.com>
2026-02-27 00:26:04 +0530

    using TokenDTOResponse directly will correct it later

--- src/main/java/com/swagger/p1/DTO/AuthResponseDTO.java
+++ src/main/java/com/swagger/p1/DTO/AuthResponseDTO.java
@@ -1,15 +1,18 @@
 package com.swagger.p1.DTO;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class AuthResponseDTO {
 
-    private String token;
+   // private String token;
 
-    public AuthResponseDTO(String token) {
-        this.token = token;
-    }
+	private TokenDTOResponse TokenDTOResponse;
+//    public AuthResponseDTO(String token) {
+//        this.token = token;
+//    }
     
 
 }
