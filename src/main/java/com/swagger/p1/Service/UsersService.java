c37d1df0f617969f107ab38f3425b16bd7fb7589
Geeta Khatri <geetakhatri746@gmail.com>
2026-07-31 20:30:24 +0530

    blacklist and logout function

--- src/main/java/com/swagger/p1/Service/UsersService.java
+++ src/main/java/com/swagger/p1/Service/UsersService.java
@@ -1,13 +1,22 @@
 package com.swagger.p1.Service;
 
+import java.time.Duration;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.swagger.p1.DTO.UsersDTO;
 import com.swagger.p1.Entity.Users;
+import com.swagger.p1.config.RedisConfig;
 import com.swagger.p1.repository.*;
+import com.swagger.p1.security.JWTConfig;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 @Service
 public class UsersService {
 
@@ -16,6 +25,16 @@ public class UsersService {
 
     @Autowired
     private  UsersRepo urepo;
+    
+    @Autowired
+    private JWTConfig jwtConfig;
+    
+    @Autowired
+    private RedisConfig redisConfig;
+    
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
 
     public ResponseEntity<String>  registreUser(UsersDTO uu){
         if(uu.getUsername()==null || uu.getUsername().trim().isEmpty()){
@@ -41,9 +60,34 @@ public class UsersService {
     }
     }
     // public ResponseEntity<?> loginUser(authRequestDTO authreq){
+
+	public ResponseEntity<String> logout(String token) {
+		
+		String uname=jwtConfig.validateToken(token);
+		if(!uname.isEmpty()) {
+			Date expirationDate  =jwtConfig.etxractClaims(token).getBody().getExpiration();
+			
+			long remainingMillis = expirationDate.getTime()-System.currentTimeMillis();
+			System.out.println("remainingMillis is "+remainingMillis);
+			if(remainingMillis>0) {
+				blacklist(token,remainingMillis);
+			}
+			
+		}
+		
+		return   ResponseEntity.ok("Logged out successfully");
+	}
+	
+	
+	private void blacklist(String token,long  d) {
+		System.out.println("inside blacklist "+d);
+		redisTemplate.opsForValue()
+        .set(token, "BLACKLISTED", Duration.ofMillis(d));
+		System.out.println("redisTemplate is "+redisTemplate);
+	}
         
 
-    // }
+   
 
    
 }
